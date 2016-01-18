2495ae8fe882f3d2e10cdf70e66566221cbab468
Katarzyna Starachowicz <kstarachowicz@gmail.com>
2016-01-18 12:40:12 +0100

    send direct messages

--- twitter_spambot/micro_blogger.rb
+++ twitter_spambot/micro_blogger.rb
@@ -16,6 +16,29 @@ class MicroBlogger
     end
   end
 
+  def dm(target, message)
+    puts "Trying to send #{target} this direct message:"
+    puts message
+    screen_names = @client.followers.collect { |follower| @client.user(follower).screen_name}
+    if screen_names.include? target
+      message = "d @#{target} #{message}"
+      tweet(message)
+    else
+      puts "Error! You can only DM people who follow you."
+    end
+  end
+
+  def followers_list
+    screen_names = []
+    @client.followers.each { |follower| screen_names << @client.user(follower).screen_name }
+    screen_names
+  end
+
+  def spam_my_followers(message)
+    screen_names = followers_list
+    screen_names.each { |follower| dm(follower, message)}
+  end
+
   def run
     puts "Welcome to the JSL Twitter Client!"
     command = ""
@@ -27,6 +50,8 @@ class MicroBlogger
       case command
         when 'q' then puts "Goodbye!"
         when 't' then tweet(parts[1..-1].join(" "))
+        when 'dm' then dm(parts[1], parts[2..-1].join(" "))
+        when 'spam' then spam_my_followers(parts[1..-1].join(" "))
         else
           puts "Sorry, I don't know how to #{command}."
       end
