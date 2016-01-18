25aa2f48168648012918f748e4cae4f33e5ce2de
Katarzyna Starachowicz <kstarachowicz@gmail.com>
2016-01-18 13:50:03 +0100

    shorten URLs with Bit.ly

--- twitter_spambot/micro_blogger.rb
+++ twitter_spambot/micro_blogger.rb
@@ -1,4 +1,5 @@
 require 'jumpstart_auth'
+require 'bitly'
 
 class MicroBlogger
   attr_reader :client
@@ -52,6 +53,13 @@ class MicroBlogger
     end
   end
 
+  def shorten(original_url)
+    puts "Shortening this URL: #{original_url}"
+    Bitly.use_api_version_3
+    bitly = Bitly.new('hungryacademy', 'R_430e9f62250186d2612cca76eee2dbc6')
+    return bitly.shorten(original_url).short_url
+  end
+
   def run
     puts "Welcome to the JSL Twitter Client!"
     command = ""
@@ -66,6 +74,8 @@ class MicroBlogger
         when 'dm' then dm(parts[1], parts[2..-1].join(" "))
         when 'spam' then spam_my_followers(parts[1..-1].join(" "))
         when 'elt' then everyones_last_tweet
+        when 's' then shorten(url)
+        when 'turl' then tweet(parts[1..-2].join(" ") + " " + shorten(parts[-1]))
         else
           puts "Sorry, I don't know how to #{command}."
       end
